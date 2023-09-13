fb4d621a085cd313ed23d90dcdaebc3334c60d03
Pedro Lucas Carolino Felix <pedro.carolino.develop@gmail.com>
2023-09-12 23:08:30 -0300

    Primeiro commit com as introduções

---
+++ 1_Introdução/hello-world_vite/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p className="paragrafo">Aqui é um paragrafo adicionado para o primeiro tarefa do curso</p>
+    </>
+  );
+}
+
+export default App;
